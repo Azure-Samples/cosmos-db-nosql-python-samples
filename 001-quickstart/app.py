a99174f716f16526abbbd5f0e121f348f174d467
Sidney Andrews <sidandrews@microsoft.com>
2022-11-03 18:35:28 -0400

    Update primary code sample

--- 001-quickstart/app.py
+++ 001-quickstart/app.py
@@ -1,16 +1,27 @@
+# <imports>
 from azure.cosmos import CosmosClient, PartitionKey
 
+# </imports>
+
+# <environment_variables>
 import os
 
 endpoint = os.environ["COSMOS_ENDPOINT"]
 key = os.environ["COSMOS_KEY"]
+# </environment_variables>
 
+# <create_client>
 client = CosmosClient(url=endpoint, credential=key)
+# </create_client>
 
+# <create_database>
 database = client.create_database_if_not_exists(id="adventureworks")
+# </create_database>
 
+# <create_container>
 partitionKeyPath = PartitionKey(path="/categoryId")
 
 container = database.create_container_if_not_exists(
     id="products", partition_key=partitionKeyPath, offer_throughput=400
 )
+# </create_container>
