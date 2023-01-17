ee0a5ae6b580fe1d2875d77a2c44fce8dc860fdf
Megha Anand <88059806+anandmeg@users.noreply.github.com>
2023-01-17 13:41:41 -0800

    Update app.py
    Adding Python async code

--- 002-quickstart-async/app.py
+++ 002-quickstart-async/app.py
@@ -1,68 +1,56 @@
 # <imports>
 import os
 import json
-from azure.cosmos import CosmosClient, PartitionKey
+import asyncio
 
-# </imports>
+from azure.cosmos import PartitionKey
+from azure.cosmos.aio import CosmosClient
 
-# <environment_variables>
-endpoint = os.environ["COSMOS_ENDPOINT"]
-key = os.environ["COSMOS_KEY"]
-# </environment_variables>
+ENDPOINT = os.environ["COSMOS_ENDPOINT"]
+KEY = os.environ["COSMOS_KEY"]
+DATABASE_NAME = 'cosmicworks'
+CONTAINER_NAME = 'products'
 
-# <create_client>
-client = CosmosClient(url=endpoint, credential=key)
-# </create_client>
+async def manage_cosmos():
+    async with CosmosClient(url=ENDPOINT, credential=KEY) as client:
+        database = await client.create_database_if_not_exists(id=DATABASE_NAME)
+        print("Database", database)
+        
+        partition_key_path = PartitionKey(path="/categoryId")
+        
+        container = await database.create_container_if_not_exists(
+            id=CONTAINER_NAME, partition_key=partition_key_path, offer_throughput=400
+        )
+        print("Container", container)
+        
+        new_item = {
+          "id": "70b63682-b93a-4c77-aad2-65501347265f",
+          "categoryId": "61dba35b-4f02-45c5-b648-c6badc0cbd79",
+          "categoryName": "gear-surf-surfboards",
+          "name": "Yamba Surfboard",
+          "quantity": 12,
+          "sale": False,
+        }
+        
+        await container.create_item(newItem)
+        
+        existing_item = await container.read_item(
+            item="70b63682-b93a-4c77-aad2-65501347265f",
+            partition_key="61dba35b-4f02-45c5-b648-c6badc0cbd79",
+        )
+        print("Existing item", existing_item)
+        if not existing_item:
+          created_item = await container.create_item(new_item)
+          print("Created item", created_item)
 
-# <create_database>
-database = client.create_database_if_not_exists(id="cosmicworks")
-# </create_database>
+        QUERY = "SELECT * FROM products p WHERE p.categoryId = @categoryId"
+        CATEGORYID = "61dba35b-4f02-45c5-b648-c6badc0cbd79"
+        params = [dict(name="@categoryId", value=CATEGORYID)]
+        
+        results = container.query_items(
+            query=QUERY, parameters=params, enable_cross_partition_query=None
+            )
+        item_list = [item async for item in results]
+        print("Item list", item_list)
 
-# <create_partition_key>
-partitionKeyPath = PartitionKey(path="/categoryId")
-# </create_partition_key>
-
-# <create_container>
-container = database.create_container_if_not_exists(
-    id="products", partition_key=partitionKeyPath, offer_throughput=400
-)
-# </create_container>
-
-# <new_item>
-newItem = {
-    "id": "70b63682-b93a-4c77-aad2-65501347265f",
-    "categoryId": "61dba35b-4f02-45c5-b648-c6badc0cbd79",
-    "categoryName": "gear-surf-surfboards",
-    "name": "Yamba Surfboard",
-    "quantity": 12,
-    "sale": False,
-}
-# </new_item>
-
-# <create_item>
-container.create_item(newItem)
-# </create_item>
-
-# <read_item>
-existingItem = container.read_item(
-    item="70b63682-b93a-4c77-aad2-65501347265f",
-    partition_key="61dba35b-4f02-45c5-b648-c6badc0cbd79",
-)
-# </read_item>
-
-# <build_query>
-QUERY = "SELECT * FROM products p WHERE p.categoryId = @categoryId"
-CATEGORYID = "61dba35b-4f02-45c5-b648-c6badc0cbd79"
-params = [dict(name="@categoryId", value=CATEGORYID)]
-# </build_query>
-
-# <query_items>
-items = container.query_items(
-    query=QUERY, parameters=params, enable_cross_partition_query=False
-)
-# </query_items>
-
-# <iterate_query_results>
-for item in items:
-    print(json.dumps(item, indent=True))
-# </iterate_query_results>
+asyncio.run(manage_cosmos())
