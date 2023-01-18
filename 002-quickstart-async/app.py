2b0be6a150b05683b9533712681d234cb6b9183d
Sidney Andrews <sidandrews@microsoft.com>
2023-01-18 20:58:00 +0000

    Formatting updates

--- 002-quickstart-async/app.py
+++ 002-quickstart-async/app.py
@@ -1,6 +1,5 @@
 # <imports>
 import os
-import json
 import asyncio
 
 from azure.cosmos import PartitionKey
@@ -14,49 +13,55 @@ KEY = os.environ["COSMOS_KEY"]
 # </environment_variables>
 
 # <constants>
-DATABASE_NAME = 'cosmicworks'
-CONTAINER_NAME = 'products'
+DATABASE_NAME = "cosmicworks"
+CONTAINER_NAME = "products"
 # </constants>
 
 # <async_code>
+
+
 async def manage_cosmos():
+
     async with CosmosClient(url=ENDPOINT, credential=KEY) as client:
+
         database = await client.create_database_if_not_exists(id=DATABASE_NAME)
         print("Database", database)
-        
-        
-        partition_key_path = PartitionKey(path="/categoryId")
-        
+
+        key_path = PartitionKey(path="/categoryId")
+
         container = await database.create_container_if_not_exists(
-            id=CONTAINER_NAME, partition_key=partition_key_path, offer_throughput=400
+            id=CONTAINER_NAME, partition_key=key_path, offer_throughput=400
         )
         print("Container", container)
-        
+
         new_item = {
-          "id": "70b63682-b93a-4c77-aad2-65501347265f",
-          "categoryId": "61dba35b-4f02-45c5-b648-c6badc0cbd79",
-          "categoryName": "gear-surf-surfboards",
-          "name": "Yamba Surfboard",
-          "quantity": 12,
-          "sale": False,
+            "id": "70b63682-b93a-4c77-aad2-65501347265f",
+            "categoryId": "61dba35b-4f02-45c5-b648-c6badc0cbd79",
+            "categoryName": "gear-surf-surfboards",
+            "name": "Yamba Surfboard",
+            "quantity": 12,
+            "sale": False,
         }
-        
-         created_item = await container.create_item(new_item)
-        
+
+        await container.create_item(new_item)
+
         existing_item = await container.read_item(
             item="70b63682-b93a-4c77-aad2-65501347265f",
             partition_key="61dba35b-4f02-45c5-b648-c6badc0cbd79",
         )
+        print("Point read item", existing_item["name"])
 
         QUERY = "SELECT * FROM products p WHERE p.categoryId = @categoryId"
         CATEGORYID = "61dba35b-4f02-45c5-b648-c6badc0cbd79"
         params = [dict(name="@categoryId", value=CATEGORYID)]
-        
+
         results = container.query_items(
             query=QUERY, parameters=params, enable_cross_partition_query=None
-            )
+        )
         item_list = [item async for item in results]
         print("Item list", item_list)
+
+
 # </async_code>
 
 # (run_async_function)
