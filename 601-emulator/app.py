508133300c1a1b3c82640c67d579d090851bb99c
Sidney Andrews <sidandrews@microsoft.com>
2023-09-13 18:45:57 +0000

    Implement emulator sample

---
+++ 601-emulator/app.py
@@ -0,0 +1,34 @@
+# <imports>
+from azure.cosmos import CosmosClient, PartitionKey
+
+# </imports>
+
+# <client>
+client = CosmosClient(
+    url="<https://localhost:8081>",
+    credential=(
+        "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGG"
+        "yPMbIZnqyMsEcaGQy67XIw/Jw=="
+    ),
+)
+# </client>
+
+# <resources>
+database = client.create_database_if_not_exists(
+    id="cosmicworks",
+    offer_throughput=400,
+)
+
+container = database.create_container_if_not_exists(
+    id="products",
+    partition_key=PartitionKey(
+        path="/id",
+    ),
+)
+# </resources>
+
+# <upsert>
+item = {"id": "68719518371", "name": "Kiama classic surfboard"}
+
+container.upsert_item(item)
+# </upsert>
